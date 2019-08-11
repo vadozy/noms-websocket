fd928d3b6b55e1c8cb94e267173d34a2334f7b7e
Vadim Storozhuk <vadim.storo@gmail.com>
2019-08-11 12:43:00 -0400

    socket.io client

--- src/components/prices/Prices.js
+++ src/components/prices/Prices.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import Table from "./Table";
+import socketIOClient from "socket.io-client";
 const price_generator = require("../../util/mock/prices_generator");
+
+const websocket_server = "http://127.0.0.1:4242";
 
 class Prices extends Component {
   state = {
     prices: []
   };
 
-  componentDidMount() {
+  componentDidMount2() {
     this.setState({
       prices: price_generator()
     });
@@ -19,6 +22,17 @@ class Prices extends Component {
         this.setState({...this.state});
       }, 9000);
     }, 10000);
+  }
+
+  componentDidMount() {
+    const socket = socketIOClient(websocket_server);
+    //socket.on("prices-API", data => console.log(data.length));
+    socket.on("prices-API", data => {
+      this.setState({ prices: JSON.parse(data) });
+      setTimeout(() => {
+        this.setState({...this.state});
+      }, 9000);
+    });
   }
 
   render() {
